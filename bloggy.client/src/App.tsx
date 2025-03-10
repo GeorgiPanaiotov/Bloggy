bf76b9c16c533b90e1819b75b8dc4e096bc031d0
GeorgiPanaiotov <rippness@gmail.com>
2025-03-10 12:28:35 +0200

    Add project files.

---
+++ bloggy.client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        <h1>Bloggy</h1>
+    );
+}
+
+export default App;
